dfc209b8353e51d44d2303d0f6f13ae3b22dca75
Anjani Som <137597138+Anjanisom0213@users.noreply.github.com>
2025-05-29 18:16:02 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter first number: "))
+
+num2 = float(input("Enter second number: "))
+
+op = input("Enter operator (+, -, *, /): ")
+
+if op == "+":5
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator!")
